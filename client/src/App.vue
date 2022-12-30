5c32b9a55c9ce9854c2951aaa4330d656b551589
osman <Osman Beyhan>
2022-12-31 00:52:23 +0300

    New commit

--- client/src/App.vue
+++ client/src/App.vue
@@ -1,7 +1,7 @@
-<script setup>
-import { RouterLink, RouterView } from 'vue-router'
-import Navbar from './components/Navbar.vue'
-import Popup from './components/Popup.vue'
+<script setup lang="ts">
+  import { RouterLink, RouterView } from 'vue-router'
+  import Navbar from './components/Navbar.vue'
+  import Popup from './components/Popup.vue'
 
 </script>
 

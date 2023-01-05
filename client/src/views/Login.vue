ee5f769d14b255d340536c670a8292e4f272c304
Osman Beyhan <osmanbeyhan12@gmail.com>
2023-01-05 22:11:19 +0300

    I started front-end for API

--- client/src/views/Login.vue
+++ client/src/views/Login.vue
@@ -1,4 +1,9 @@
 <script setup>
+  import axios from "axios";
+  import { onMounted, ref } from "vue";
+  const username = ref('')
+  const password = ref('')
+  const apiUrl = "http://localhost:3000";
   function togglePassword() {
       var element = document.getElementById('password');
       var eye = document.getElementById('eye');
@@ -10,7 +15,9 @@
         eye.setAttribute('class', 'bi bi-eye-slash-fill')
       }
   }
-  
+  async function Login() {
+    const response = await axios.post(`${apiUrl}/login`, {username: username.value, password: password.value})
+  }
 </script>
 <template>
   <form class="">
@@ -21,7 +28,7 @@
       <span class="input-group-text" id="basic-addon1">
         <i class="bi bi-person-fill"></i>
       </span>
-      <input type="text" id="username" class="form-control " style="outline:none;" required autofocus placeholder="Username" aria-label="Username" aria-describedby="basic-addon1">
+      <input type="text" id="username" v-model="username" class="form-control " style="outline:none;" required autofocus placeholder="Username" aria-label="Username" aria-describedby="basic-addon1">
     </div>
 
     <label for="password">Password</label>
@@ -29,12 +36,12 @@
       <span class="input-group-text">
         <i class="bi bi-key-fill"></i>
       </span>
-      <input type="password" class="form-control" required placeholder="Password" id="password">
+      <input type="password" v-model="password" class="form-control" required placeholder="Password" id="password">
       <span class="input-group-text" @click="togglePassword()">
         <i class="bi bi-eye-fill" id="eye"></i>
       </span>
     </div>
-    <button type="submit" name="button" class="btn btn-warning text-center input-group">LOGIN</button>
+    <button type="submit" @click="Login()" name="button" class="btn btn-warning text-center input-group">LOGIN</button>
     <br><br>
     <p class="text-center">
       <a id="forgot-password" href="/forgot-password" class="text-info">Forgot Password?</a>
